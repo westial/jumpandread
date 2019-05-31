c805c6fa789ce6c87ac292db109eaece5dff61a9
jaume <jmila@leadtech.com>
2019-05-31 11:07:49 +0900

    59-entry-with-description-of-the-number-of-results-for-the-terms-as FIX pause after results entry

--- src/main/java/com/westial/alexa/jumpandread/application/SearchUseCase.java
+++ src/main/java/com/westial/alexa/jumpandread/application/SearchUseCase.java
@@ -56,6 +56,7 @@ public class SearchUseCase {
                     );
                 }
 
+                presenter.addText(Presenter.LONGEST_TOKEN);
                 presenter.addText(listing);
 
             }
